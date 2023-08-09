df4ab5928e10a04cc6c13df349faeb852d15e4a9
lawrenceyim <lawrence.yim.010@gmail.com>
2023-08-09 11:47:56 -0400

    Deleted intro letter scene to undo crash

--- Assets/Scripts/UI/CookingUI.cs
+++ Assets/Scripts/UI/CookingUI.cs
@@ -9,11 +9,16 @@ public class CookingUI : MonoBehaviour
     [SerializeField] Controller controller;
     [SerializeField] GameObject[] cookingSlots;
     [SerializeField] SpriteRenderer[] iconSpriteRenderers;
+    SpriteRenderer[] buttonSpriteRenderers;
     TextMeshPro[] buttonKeys;
     Dictionary<KeyCode, Recipe.Ingredients> availableKeys;
     int currentIndex;
     List<Recipe.Ingredients> neededForDish;
     Dish dish;
+    Dictionary<KeyCode, int> keycodeIndex;
+    bool[,] grayKeys;
+    [SerializeField] Color originalColor;
+
 
     void Start()
     {
@@ -22,7 +27,10 @@ public class CookingUI : MonoBehaviour
         }
         availableKeys = new Dictionary<KeyCode, Recipe.Ingredients>();
         buttonKeys = new TextMeshPro[cookingSlots.Length];
+        buttonSpriteRenderers = new SpriteRenderer[cookingSlots.Length];
+        grayKeys = new bool[6, cookingSlots.Length];
         for (int i = 0; i < cookingSlots.Length; i++) {
+            buttonSpriteRenderers[i] = cookingSlots[i].GetComponent<SpriteRenderer>();
             buttonKeys[i] = cookingSlots[i].transform.Find("Button").transform.Find("Text").GetComponent<TextMeshPro>();
         }
         DeactivateButtons();
@@ -38,10 +46,16 @@ public class CookingUI : MonoBehaviour
         currentIndex = index;
         availableKeys = Recipe.GetCurrentKeys(dish.foodType);
         List<Recipe.Ingredients> ingredients = Recipe.GetAllIngredients(dish);
+        keycodeIndex = new Dictionary<KeyCode, int>();
         for (int i = 0; i < ingredients.Count; i++) {
+            buttonSpriteRenderers[i].color = Color.red;
             iconSpriteRenderers[i].sprite = PrefabCache.instance.iconDict[ingredients[i]];
             iconSpriteRenderers[i].enabled = true;
             buttonKeys[i].text = Recipe.keyMapping[ingredients[i]].ToString();
+            keycodeIndex.Add(Recipe.keyMapping[ingredients[i]], i);
+            if (grayKeys[currentIndex, i]) {
+                buttonSpriteRenderers[i].color = Color.gray;
+            }
         }
         neededForDish = dish.ingredientsList;
     }
@@ -50,6 +64,7 @@ public class CookingUI : MonoBehaviour
         for (int i = 0; i < cookingSlots.Length; i++) {
             iconSpriteRenderers[i].enabled = false;
             buttonKeys[i].text = "";
+            buttonSpriteRenderers[i].color = originalColor;
         }
         availableKeys = null;
     }
@@ -58,6 +73,9 @@ public class CookingUI : MonoBehaviour
         if (availableKeys == null) return;
         if (controller.IsDishComplete()) {
             if (Input.GetKeyDown(KeyCode.Space)) {
+                for (int i = 0; i < cookingSlots.Length; i++) {
+                    grayKeys[currentIndex, i] = false;
+                }
                 controller.ServeTheDish();
             }
             return;
@@ -69,15 +87,21 @@ public class CookingUI : MonoBehaviour
                     AudioManager.instance.PlayTrashSound();
                     Debug.Log("Wrong ingredient. Dish scrapped");
                     controller.ResetDishBeingWorkedOn(currentIndex);
+                    for (int i = 0; i < availableKeys.Count; i++) {
+                        buttonSpriteRenderers[i].color = Color.red;
+                        grayKeys[currentIndex, i] = false;
+                    }
                 } 
-                if (!PlayerData.HasEnoughMoney(Recipe.ingredientCost[ingredient])) {
+                else if (!PlayerData.HasEnoughMoney(Recipe.ingredientCost[ingredient])) {
                     continue;
                 }
-                if (!controller.IngredientAlreadyAdded(ingredient)) {
+                else if (!controller.IngredientAlreadyAdded(ingredient)) {
                     AudioManager.instance.PlayAddIngredientSound();
                     PlayerData.money -= Recipe.ingredientCost[ingredient];
                     controller.UpdateMoneyUI();
                     controller.AddIngredientToDish(ingredient);
+                    buttonSpriteRenderers[keycodeIndex[key]].color = Color.gray;
+                    grayKeys[currentIndex, keycodeIndex[key]] = true;
                     continue;
                 } 
             }
