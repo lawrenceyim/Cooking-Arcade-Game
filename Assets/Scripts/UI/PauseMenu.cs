60691854b64cce1269b3b48c812e0bbfc0762bed
lawrenceyim <lawrence.yim.010@gmail.com>
2023-09-09 09:38:32 -0400

    Changed pause menu from UI to sprites to fix visual bug

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -7,7 +7,6 @@ using UnityEngine.UI;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
-    bool paused;
     
     private void Start() {
         pauseMenu.SetActive(false);
@@ -16,12 +15,10 @@ public class PauseMenu : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape)) {
-            if (paused) {
-                paused = false;
+            if (Time.timeScale == 0f) {
                 Time.timeScale = 1f;
                 pauseMenu.SetActive(false);
             } else {
-                paused = true;
                 Time.timeScale = 0f;
                 pauseMenu.SetActive(true);
             }
@@ -29,7 +26,6 @@ public class PauseMenu : MonoBehaviour
     }
 
     public void ResumeGame() {
-        paused = false;
         Time.timeScale = 1f;
         pauseMenu.SetActive(false);
     }
---
+++ Assets/Scripts/UI/SpriteButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpriteButton : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    [SerializeField] string functionName;
+    private void OnMouseDown() {
+        if (functionName == "Return") {
+            ReturnToMainMenu();
+        } else if (functionName == "Unpause") {
+            Time.timeScale = 1f;
+            pauseMenu.SetActive(false);
+        } else if (functionName == "Exit") {
+            Application.Quit();
+        }
+    }
+
+    public void ReturnToMainMenu() {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
